ae0bee7ab54b407d73c1aa96ded93471ebb268eb
juanjuanjks <juanjuanjks@163.com>
2022-11-15 19:49:04 -0600

    created layout file for ReportTruckActivity

---
+++ app/src/main/java/edu/illinois/cs465/wheresmytruck/ReportTruckActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.wheresmytruck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportTruckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_truck);
+    }
+}
