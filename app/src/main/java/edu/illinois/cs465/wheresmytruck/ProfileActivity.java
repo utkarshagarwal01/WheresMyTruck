eb700e4f6d1f1f0883a5236664d5a470d1228c77
Sean <seanduffy797@gmail.com>
2022-11-17 13:35:15 -0600

    Full layout for login, register, and profile

---
+++ app/src/main/java/edu/illinois/cs465/wheresmytruck/ProfileActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.wheresmytruck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/edu/illinois/cs465/wheresmytruck/RegisterActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.wheresmytruck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
